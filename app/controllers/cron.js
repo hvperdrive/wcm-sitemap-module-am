7a7e78427b5ac30350658ce8819f1bea25f16aa8
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-05-19 11:18:42 +0200

    feat: added medium based sitemap generator

--- app/controllers/cron.js
+++ app/controllers/cron.js
@@ -18,10 +18,12 @@ module.exports.init = () => {
 		cronTime: variables.cron,
 		onTick: () => {
 			console.log("CRON: GENERATING SITEMAP."); // eslint-disable-line no-console
-			return sitemapGenerator()
-				.then(() => console.log("CRON: SITEMAP GENERATED!"), // eslint-disable-line no-console
+			return Promise.all([
+				sitemapGenerator("am"),
+				sitemapGenerator("dgv")
+			]).then(() => console.log("CRON: SITEMAP GENERATED!"), // eslint-disable-line no-console
 					(err) => console.log("CRON: SITEMAP GENERATION FAILED => ", err) // eslint-disable-line no-console
-				);
+			);
 		},
 		onComplete: () => console.log(null, "CRON: SITEMAP GENERATION COMPLETE!"), // eslint-disable-line no-console
 		start: false,
